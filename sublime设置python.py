aa0ffc7ef96e42fb126bc2dc9371a0bf7a8acc10
Benojan <skoj@qq.com>
2016-06-20 18:55:15 +0800

    Update sublime设置python.py

--- sublime设置python.py
+++ sublime设置python.py
@@ -1,6 +1,6 @@
 # 打开"Tools-->Build System-->New Build System"，添加如下内容并保存
 {
-  "cmd": ["/usr/python3/bin", "-u", "$file"],
+  "cmd": ["/usr/bin/python3", "-u", "$file"],
   "file_regex": "^[ ]*File \"(...*?)\", line ([0-9]*)",
   "selector": "source.python",
   "encoding": "utf-8"
