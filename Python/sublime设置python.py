2208ca5fc136b9a37d465edb574aabfc811f50f9
Benojan <skoj@qq.com>
2018-01-09 19:16:56 +0800

    test

--- Python/sublime设置python.py
+++
@@ -1,7 +0,0 @@
-# 打开"Tools-->Build System-->New Build System"，添加如下内容并保存
-{
-  "cmd": ["/usr/bin/python3", "-u", "$file"],
-  "file_regex": "^[ ]*File \"(...*?)\", line ([0-9]*)",
-  "selector": "source.python",
-  "encoding": "utf-8"
-}
